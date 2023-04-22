113c8f3302c727ff194e6ebd411fa7645cff480e
bradon <bradonleeeutah@gmail.com>
2023-04-22 15:43:22 -0500

    Update azuregroupsremovals.ps1
    verbiage change on input

--- azuregroupsremovals.ps1
+++ azuregroupsremovals.ps1
@@ -1,6 +1,6 @@
 connect-azuread
 
-$termp = (Read-Host -Prompt "Please enter the Active Directory username of the terminated user:")
+$termp = (Read-Host -Prompt "Please enter the UPN username of the user:")
 
 $O365ObjectID = (get-azureaduser -Filter "userprincipalname eq '$($termp)'").objectid
 $Azuregroups = (Get-AzureADUserMembership -objectid $O365ObjectID | where-object {$_.DirSyncEnabled -like ""}).objectid
